2e26194e5bff2c1d86fc5c6ba1adda3252a37aaf
aa6my <aa6my@kampung.xyz>
2022-07-11 10:53:59 +0800

    org.w3.clearkey

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
-
-// Encode Key ID
-$hex = $_GET["i"];            // and much more hex values as string as in your example
-$bin = hex2bin($hex);       // convert the hex values to binary data stored as a PHP string
-$keyid64 = base64_encode($bin); // remove == , replace as space
-$finalkeyid64 = str_replace('=', '', $keyid64);
-
-// Encode Key
-$hex2 = $_GET["k"];            // and much more hex values as string as in your example
-$bin2 = hex2bin($hex2);       // convert the hex values to binary data stored as a PHP string
-$key64 = base64_encode($bin2); // remove == , replace as space
-$finalkey64 = str_replace('=', '', $key64);
-
 header('Content-Type: application/json; charset=utf-8');
 $json["keys"][0]["kty"] = "oct";
-$json["keys"][0]["k"] = $finalkey64;
-$json["keys"][0]["kid"] = $finalkeyid64;
+$json["keys"][0]["k"] = str_replace('=', '', base64_encode(hex2bin($_GET["i"])));;
+$json["keys"][0]["kid"] = str_replace('=', '', base64_encode(hex2bin($_GET["k"])));
 $json["type"] = "temporary";
 
 echo json_encode($json);
