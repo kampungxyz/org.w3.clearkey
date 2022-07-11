afd35481226c73c0feb8947bf556a5e29307cddd
aa6my <aa6my@kampung.xyz>
2022-07-11 12:28:33 +0800

    org.w3.clearkey

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ $json["keys"][0]["kty"] = "oct";
 $json["keys"][0]["k"] = str_replace('=','',base64_encode(hex2bin($_GET["k"])));
 $json["keys"][0]["kid"] = str_replace('=','',base64_encode(hex2bin($_GET["i"])));
 $json["type"] = "temporary";
-echo json_encode($json);
+echo json_encode($json, JSON_UNESCAPED_SLASHES);
