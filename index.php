c817831cfb3a4eab9efc0b9e1cb9ed327081414c
BudakDesa <kitzbitz@gmail.com>
2022-07-21 14:38:06 +0000

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 $json["keys"][0]["kty"] = "oct";
-$json["keys"][0]["kid"] = str_replace('=','',base64_encode(hex2bin($_GET["i"])));
-$json["keys"][0]["k"] = str_replace('=','',base64_encode(hex2bin($_GET["k"])));
+@$json["keys"][0]["kid"] = str_replace('=','',base64_encode(hex2bin($_GET["i"])));
+@$json["keys"][0]["k"] = str_replace('=','',base64_encode(hex2bin($_GET["k"])));
 $json["type"] = "temporary";
 echo json_encode($json, JSON_UNESCAPED_SLASHES);
